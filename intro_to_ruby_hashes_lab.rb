28752c7c926b653faef69c6fd01bfdaf285fea32
Kuran V Kumar <kurankumar@gmail.com>
2019-09-29 23:33:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,6 +34,9 @@ def monopoly_with_third_tier
 	    },
 	    :names => {
 	      :reading_railroad => {},
+	      :reading_railroad => {},
+	      :reading_railroad => {},
+	      :reading_railroad => {}
 	    }
 	  } 
 	}
