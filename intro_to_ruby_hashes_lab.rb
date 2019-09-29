8b40b3ae8e96dac710282ce22b63cf29b5af6936
Kuran V Kumar <kurankumar@gmail.com>
2019-09-29 23:23:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   railroad_hash = {
-	  :railroads => {:pieces => " "} 
+	  :railroads => {:pieces => 4} 
 	}
 	railroad_hash
 end
