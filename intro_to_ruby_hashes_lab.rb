9f19cbb907f71c7e2ec9c4d9532d7bb5e7a17578
Kuran V Kumar <kurankumar@gmail.com>
2019-09-29 23:18:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,7 +2,7 @@ def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 	first_hash = {
-	  railroads: "value"
+	  railroads: 
 	}
 	first_hash
 end
